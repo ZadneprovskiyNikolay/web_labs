af115ad4fa4c5b33e862981670f3fccab3c158d5
Nikolay Zadneprovskiy <zadneprovskiywork@gmail.com>
2024-02-25 22:22:30 +0300

    feat: lab 6

--- js/index.js
+++ js/index.js
@@ -13,14 +13,67 @@
         });
         observer.observe({ type: "navigation", buffered: true });
     })
-
 })();
 
 document.addEventListener("DOMContentLoaded", function() {
+    highlightNavButton();
+    loadPosts();
+});
+
+const NUMBER_OF_POSTS = 5;
+const DAY_IN_MS = 86400000;
+
+const loadPosts = async () => {
+    const articlesList = document.getElementById("posts-ul");
+    postTimeMs = Date.now();
+    const postIds = new Set();
+
+    for (let i = 0; i < NUMBER_OF_POSTS; i++) {
+        const postId = (Date.now() + i) % (NUMBER_OF_POSTS + i) + 34;
+        if (postIds.has(postId)) {
+            i--;
+            continue;
+        }
+        postIds.add(postId);
+        console.log(`postId = ${postId}`);
+        postTimeMs = postTimeMs - DAY_IN_MS * 3 * Math.random();
+        const postTimeFormated = (new Date(postTimeMs)).toLocaleString();        
+        var post = document.createElement("li");
+        post.innerHTML = `
+        <article>
+            <h4 id="post-${postId}-header"></h4>
+            <div id="post-${postId}-content"></div>
+            <img class="loading-gif" id="post-${postId}-loading-gif" src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/images/loading.gif"></src>
+        </article>
+        `
+        
+        articlesList.appendChild(post);
+
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then(async (response) => {         
+                const postHeader = document.getElementById(`post-${postId}-header`);
+                const postContent = document.getElementById(`post-${postId}-content`);
+                const postData = await response.json();
+                postHeader.innerHTML = postTimeFormated;
+                postContent.innerHTML = postData.body;
+            })
+            .finally(() => {
+                console.log('finally');
+                document.getElementById(`post-${postId}-loading-gif`).hidden = true;
+            })
+            .catch((error) => {
+                console.log('catch');
+                console.log(error);
+                document.getElementById(`post-${postId}-content`).innerHTML = "Oops...";
+            })
+    }
+}
+
+const highlightNavButton = () => {
     for (let navLink of document.getElementsByClassName('nav-link')) {
         if (navLink.href == document.location) {
             navLink.style.textDecoration = 'underline';
             break;
         }
     };
-});
+}
